5c0a4743f1030163d5a8f3a49b5a82e715d619ca
ferhel <hellenfcastillo@gmail.com>
2025-03-07 14:33:41 -0400

    Add files via upload
    Primera versión del modelo sin error de lógica. Observaciones: accuracy casi nulo (5%.)

---
+++ constants.py
@@ -0,0 +1,52 @@
+import os
+import cv2
+
+# SETTINGS
+MIN_LENGTH_FRAMES = 5
+LENGTH_KEYPOINTS = 1662
+MODEL_FRAMES = 15
+
+# PATHS
+ROOT_PATH = os.getcwd()
+FRAME_ACTIONS_PATH = os.path.join(ROOT_PATH, "frame_actions")
+DATA_PATH = os.path.join(ROOT_PATH, "data")
+DATA_JSON_PATH = os.path.join(DATA_PATH, "data.json")
+MODEL_FOLDER_PATH = os.path.join(ROOT_PATH, "models")
+MODEL_PATH = os.path.join(MODEL_FOLDER_PATH, f"actions_{MODEL_FRAMES}.keras")
+KEYPOINTS_PATH = os.path.join(DATA_PATH, "keypoints")
+WORDS_JSON_PATH = os.path.join(MODEL_FOLDER_PATH, "words.json")
+
+# SHOW IMAGE PARAMETERS
+FONT = cv2.FONT_HERSHEY_PLAIN
+FONT_SIZE = 1.5
+FONT_POS = (5, 30)
+
+words_text = {
+    "a": "A",
+    "b": "B",
+    "c": "C",
+    "d": "D",
+    "e": "E",
+    "f": "F",
+    "g": "G",
+    "h": "H",
+    "i": "I",
+    "j": "J",
+    "k": "K",
+    "l": "L",
+    "m": "M",
+    "n": "N",
+    "ñ": "Ñ",
+    "o": "O",
+    "p": "P",
+    "q": "Q",
+    "r": "R",
+    "s": "S",
+    "t": "T",
+    "u": "U",
+    "v": "V",
+    "w": "W",
+    "x": "X",
+    "y": "Y",
+    "z": "Z",
+}
